9ea8a2e427a75c920250e7d3ee3bbe3c2412c718
Alasdair-Munday <alasdair.munday@googlemail.com>
2014-02-17 18:19:04 +0000

    Added a test class for the top level of the smart trolley client app

---
+++ SmartTrolley/src/client/SmartTrolleyTests.java
@@ -0,0 +1,41 @@
+package client;
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * SmartTrolley
+ *
+ * @file	SmartTrolleyTests.java
+ *
+ * @brief Tests for the top level of SmartTrolley
+ *
+ *@author Alasdair
+ *
+ *@author Checked By: Checker(s) fill here
+ *
+ *@version 0.1 [date created: 17 Feb 2014]
+ */
+
+public class SmartTrolleyTests {
+	
+	@Before
+	public void setup(){
+		
+//		TODO implement setup routines
+		
+	}
+
+	@Test
+	public void test() {
+		
+//		TODO Implement tests
+		fail("Not yet implemented");
+		
+	}
+
+}
+
+
+/**************End of SmartTrolleyTests.java**************/
