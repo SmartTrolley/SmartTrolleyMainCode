8efe1d2327872e696ffe3f53cbcf9a1dcca6a4af
Prashant <pc653@york.ac.uk>
2014-02-24 23:00:26 +0000

    Test Commit Message

---
+++ SmartTrolley/src/server/Server.java
@@ -0,0 +1,34 @@
+/**
+* SmartTrolley
+*
+* Server Class to run the server
+*
+* @author Alick Jacklin
+* @author Prashant Chakravarty
+*
+* @author Checked By: Checker(s) fill here
+*
+* @version V0.1 [Date Created: 24 Feb 2014]
+*/
+
+//TODO We will start this with a stub to make sure we can connect to a client
+package server;
+
+public class Server {
+
+	/**
+	 *Method/Test Description
+	 *<p>Test(s)/User Story that it satisfies
+	 *@param args
+	 *[If applicable]@see [Reference URL OR Class#Method]
+	 *<p> Date Modified: 24 Feb 2014
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+/**************End of Server.java**************/
+
