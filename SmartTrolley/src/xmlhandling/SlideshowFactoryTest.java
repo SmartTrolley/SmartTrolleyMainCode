fd1486360a76fec76a26a010544c1198e8e96c46
Alasdair-Munday <alasdair.munday@googlemail.com>
2014-02-25 21:53:42 +0000

    sorted out some formating and comments on slideshowfactorytest

--- SmartTrolley/src/xmlhandling/SlideshowFactoryTest.java
+++ SmartTrolley/src/xmlhandling/SlideshowFactoryTest.java
@@ -1,20 +1,4 @@
-/**
-* SmartTrolley
-*
-* @file	SlideshowTest.java
-*
-* @brief A DESCRIPTION OF THE FILE
-*
-*@author Name1
-*@author Name2
-*
-*@author Checked By: Checker(s) fill here
-*
-*@version version of this file [date created: 24 Feb 2014]
-*/
 
-
-/**************End of SlideshowTest.java**************/
 package xmlhandling;
 
 import static org.junit.Assert.*;
@@ -22,10 +6,6 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
-/**
- * @author Alasdair Munday
- *
- */
 
 public class SlideshowFactoryTest {
 	private SlideshowFactory testSlideshowFactory;
@@ -35,17 +15,19 @@ public class SlideshowFactoryTest {
 	public void setUp()	{
 		// create a new slideshow factory
 		testSlideshowFactory = new SlideshowFactory();
-		
-		
-
 	}
 
+	/**
+	*Test that the xml processor can read in PWS documents
+	*<p>User story: User Loads shopping list from xml document
+	*<p> Date Modified: 25 Feb 2014
+	*/
 	@Test
 	public void pwsAcceptanceTest() {
 		
 //		Read in the project wide specification style test slideshow XML document
 		try {
-			pwsTestSlideshow = testSlideshowFactory.read("XMLDocs/MMPE_Schema_v0.4.xml");
+			pwsTestSlideshow = testSlideshowFactory.read("XMLDocs/pwsSlideshow.xml");
 		} catch (Exception e) {
 			System.out.println("Xml Read Error");
 			e.printStackTrace();
@@ -53,11 +35,14 @@ public class SlideshowFactoryTest {
 		
 //		check that the created class is a slide show
 		assertTrue(pwsTestSlideshow instanceof Slideshow);  
-		
-		
-		
 	}
 	
+	/**
+	*Test that the xml processor can read in smartTrolley shopping list XMLs
+	*<p> User story: User Loads shopping list from xml document
+	*
+	*<p> Date Modified: 25 Feb 2014
+	*/
 	@Test
 	public void smartTrolleyFileAcceptanceTest(){
 		
