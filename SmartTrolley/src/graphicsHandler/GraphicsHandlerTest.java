7588aa64f828591024e82e45c1e1a56f70c4cc34
Matt <mw823@york.ac.uk>
2014-03-06 14:42:23 +0000

    Functionality for creating a square tested and passed using junit, visual tests for square still to be performed.
    Tests for colours, circle and pentagon still to be performed.

--- SmartTrolley/src/graphicsHandler/GraphicsHandlerTest.java
+++ SmartTrolley/src/graphicsHandler/GraphicsHandlerTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import java.util.PriorityQueue;
 
+import javafx.geometry.Bounds;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Polygon;
 import javafx.scene.shape.Shape;
@@ -13,22 +14,40 @@ import org.junit.Test;
 
 public class GraphicsHandlerTest {
 
-	PriorityQueue<ShapePoint> points;
+	public PriorityQueue<ShapePoint> points;
 	int width = 50, height = 50, pointLow = 0, pentagonX = 25, pentagonY = 25;
+	int numberOfPointsThatASquareHas =4, pentagonNumberPoints = 5, one = 1;
 	int point1Num = 1, point2Num = 2, point3Num = 3, point4Num = 4, point5Num = 5;
-	SlideShapeFactory shapeFactory;
-	ShapePoint point1, point2, point3, point4, point5, testingPoint;
-	Shape square, pentagon, circle;
-
+	public SlideShapeFactory shapeFactory;
+	public ShapePoint point1, point2, point3, point4, point5, testingPoint;
+	public Shape square, pentagon, circle;
 
 	@Before
 	public void setUp() throws Exception {
-
 		point1 = new ShapePoint(pointLow,pointLow,point1Num);
 		point2 = new ShapePoint(width,pointLow,point2Num);
 		point3 = new ShapePoint(width,height,point3Num);
 		point4 = new ShapePoint(pointLow,width,point4Num);
 		point5 = new ShapePoint(pentagonX, pentagonY, point5Num);
+		
+	}
+	
+	@Before
+	public void squareSetUp() throws Exception{
+		
+		points = new PriorityQueue<ShapePoint>();
+		
+		//set points with the first 4 ShapePoints to create a square
+		points.add(point1);
+		points.add(point3);
+		points.add(point2);
+		points.add(point4);
+		
+		//Instantiate a shapeFactory with the current values
+		shapeFactory = new SlideShapeFactory(points, width, height);
+		
+		//get the shape that is created by the values previously given to the factory
+		square = shapeFactory.getShape();
 		
 	}
 	
@@ -41,21 +60,6 @@ public class GraphicsHandlerTest {
 	*/		
 	@Test
 	public void squareClassTest() {
-		points = new PriorityQueue<ShapePoint>();
-		
-		//set points with the first 4 ShapePoints to create a square
-		points.add(point1);
-		points.add(point2);
-		points.add(point3);
-		points.add(point4);
-		
-		//Instantiate a shapeFactory with the current values
-		shapeFactory = new SlideShapeFactory(points, width, height);
-		
-
-		
-		//get the shape that is created by the values previously given to the factory
-		square = shapeFactory.getShape();
 		
 		String shapetype = square.getClass().getName();
 		// print the class of square to the console for manual test
@@ -67,11 +71,14 @@ public class GraphicsHandlerTest {
 	
 	@Test
 	public void squareHeightTest()	{
-		double squareHeight = square.getBoundsInLocal().getHeight();
-		assertEquals(squareHeight, height, 0.001);
+		Pane testPane = new Pane();
+		testPane.getChildren().add(square);
+		Bounds squareBounds = square.getBoundsInParent();
+		double squareHeight =squareBounds.getHeight();
+		assertEquals(squareHeight, height, 0.0001);
 	}
 	
-	
+	//tests that 
 	@Test
 	public void squarePointsTest()	{
 		double[] testingPointDouble;
